8d3b8f88c117079c1f7afcaea0fd007a73813987
arvin <wdwzhmm@gmail.com>
2022-09-18 19:10:54 +0800

    some changes

--- src/extension.ts
+++ src/extension.ts
@@ -5,22 +5,35 @@ import * as vscode from 'vscode';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "timelinenote" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('timelinenote.helloWorld', () => {
+	const openedPanelMap = new Map<string, boolean | undefined | null>();
+	let isFirstActivate: boolean = true;
+    let timer :any = null;
+	let disposable = vscode.commands.registerTextEditorCommand('extension.timelinenote', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from TimeLineNote!');
 	});
 
+	const executeFirstCommand = (originFileName: string) => {
+		if (isFirstActivate) {
+			isFirstActivate = false;
+			openedPanelMap.set(originFileName, true);
+			timer = setTimeout(() => {
+			vscode.commands.executeCommand("extension.timelinenote");
+			}, 300);
+		}
+	};
+	executeFirstCommand(
+		(vscode.window.activeTextEditor as vscode.TextEditor).document.fileName
+	);
 	context.subscriptions.push(disposable);
+	vscode.workspace.onDidOpenNotebookDocument(e=>{
+		console.log("in onDidOpenNotebookDocument");
+	});
+	vscode.workspace.onDidCloseTextDocument(e=>{
+		console.log("in onDidCloseTextDocument");
+	});
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
